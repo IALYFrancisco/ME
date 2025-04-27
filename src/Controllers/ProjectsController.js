2f4523597b668421e9d49b5fd8506e1c0b72e909
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-27 12:58:44 +0300

    ajout de filtre de nom de domaine dans le projet

--- src/Controllers/ProjectsController.js
+++ src/Controllers/ProjectsController.js
@@ -1,7 +1,20 @@
 const express = require('express')
+const cors = require('cors')
 const { getAllProjects, addProject } = require('../Services/ProjectsServices')
 
 const _project_router = express.Router()
+
+var allowedOrigins = []
+
+_project_router.use(cors({
+    origin: function(_origin, callback){
+        if(!_origin || allowedOrigins.includes(_origin)){
+            callback(null, true)
+        }else{
+            callback(new Error("Votre nom de domain n'est pas autorisé."))
+        }
+    }
+}))
 
 // Route en charge du récupération de la liste de touts mes projets
 _project_router.get("/get-all", getAllProjects)
