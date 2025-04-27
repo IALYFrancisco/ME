96928c3c4e216fc7946c9b2b660ae2f81cbef0a9
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-27 08:55:56 +0300

    récuperation des listes de projets mis en place

--- src/Services/ProjectsServices.js
+++ src/Services/ProjectsServices.js
@@ -1,5 +1,19 @@
-function _getAllProjects(request, response) {
-    response.json({message: "Liste de tout les projets."})
+const { Projects } = require("../Models/ProjectsModel")
+const { connexion, disconnexion } = require("./DbServices")
+
+async function _getAllProjects(request, response) {
+    try {
+        await connexion()
+        let projects = await Projects.find({})
+        response.status(200).json({
+            message: "Liste de tout les projets.",
+            data: projects
+        })
+    }catch(error){
+        console.log(error)
+    }finally{
+        await disconnexion()
+    }
 }
 
 module.exports = {
