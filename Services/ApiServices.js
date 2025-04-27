1a002fcc5d34bd579fa9c05e043e8eb8f427696b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-27 08:04:32 +0300

    création des contrôleurs et services pour l'api [ressource mes projets]

---
+++ Services/ApiServices.js
@@ -0,0 +1,9 @@
+const { model } = require("mongoose")
+
+function _getAllProjects(request, response) {
+    response.json({message: "Liste de tout les projets"})
+}
+
+module.exports = {
+    getAllProjects : _getAllProjects 
+}
--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv')
+const { api_router } = require('./Controllers/ApiController')
 
 dotenv.config()
 
 const app = express()
 
-app.listen(process.env.APP_POST, ()=> {
+app.use('/', api_router)
+
+app.listen(process.env.APP_POST, () => {
     console.log(`L'application s'exécute sur ${process.env.APP_ADDRESS}`)
 })
