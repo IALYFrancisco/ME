293675e10a741f5b40ec9ad6da574ffc082c32c7
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-27 23:19:33 +0300

    passage de la liste des clés vers la page des clés dans le dashboard

--- src/Services/DashboardServices.js
+++ src/Services/DashboardServices.js
@@ -1,9 +1,23 @@
+const { Clients } = require("../Models/ClientsModel")
+const { connexion, disconnexion } = require("./DbServices")
+
 function _getDashboard(request, response){
     response.render('Dashboard/Dashboard')
 }
 
-function _getAPIs(request, response){
-    response.render('Dashboard/APIs')
+async function _getAPIs(request, response){
+    try{
+        await connexion()
+        let _keys = await Clients.find({})
+        context = {
+            keys : _keys
+        }
+        response.render('Dashboard/APIs', context)
+    }catch(error){
+        console.log("Error getting apis page.")
+    }finally{
+        await disconnexion()
+    }
 }
 
 module.exports = {
