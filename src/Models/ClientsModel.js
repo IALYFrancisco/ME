aba6916eb1812152a4b60220befceec0efeebc42
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-27 15:13:11 +0300

    ajout de fonctionnalité de création de client

--- src/Models/ClientsModel.js
+++ src/Models/ClientsModel.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const clientsShcema = mongoose.Schema({
-    projects_id: { type: String, required: true },
+    project_id: { type: String, required: true },
     api_key: { type: String, require: true }
 })
 
--- src/Services/APIServices.js
+++ src/Services/APIServices.js
@@ -1,4 +1,7 @@
 const crypto = require('crypto')
+const { connexion, disconnexion } = require('./DbServices')
+const { Projects } = require('../Models/ProjectsModel')
+const { Clients } = require('../Models/ClientsModel')
 
 function _checkAPIKey(request, response, next){
     try{
@@ -22,20 +25,51 @@ function _checkAPIKey(request, response, next){
     }
 }
 
-function _generateAPIKey(request, response){
+async function checkProject(project_id){
     try{
-        let new_api_key = crypto.randomBytes(32).toString('hex')
-        response.status(201).json({
-            message: "Api created successfully.",
-            data: {
-                api_key: new_api_key
+        let project = await Projects.find({_id: project_id})
+        if(project){
+            return true
+        }else{ 
+            return false
+        }
+    }catch(error){
+        return false
+    }
+}
+
+async function _generateAPIKey(request, response){
+    try{
+        await connexion()
+        if(request.body && request.body.project_id){
+            if(await checkProject(request.body.project_id)){
+                let newAPIKey = await crypto.randomBytes(32).toString('hex')
+                let newClient = Clients({
+                    project_id : request.body.project_id,
+                    api_key: newAPIKey
+                })
+                await newClient.save()
+                response.status(201).json({
+                    message: "Client created successfully.",
+                    client: newClient
+                })
+            }else{
+                response.status(203).json({
+                    message: "Project doesn't exist."
+                })
             }
-        })
+        }else{
+            response.status(400).json({
+                message: "project_id is required in request body."
+            })
+        }
     }catch(_error){
         response.status(500).json({
-            message: "Error creating api key. Maybe server eror",
-            error: _error
+            message: "Error creating client. Maybe error server.",
+            error: _error 
         })
+    }finally{
+        await disconnexion()
     }
 }
 
