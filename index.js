d5b78b6f1a9af00bd8861e868985ecb098521db0
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-27 12:20:36 +0300

    checkAPIKey ne marche désormais qu'avec les endpoints de projet

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const dotenv = require('dotenv')
 const { project_router } = require('./src/Controllers/ProjectsController')
+const { checkAPIKey } = require('./src/Services/APIServices')
 
 dotenv.config()
 
@@ -8,7 +9,7 @@ const app = express()
 
 app.use(express.json())
 
-app.use('/project', project_router)
+app.use('/project', checkAPIKey, project_router)
 
 app.listen(process.env.APP_PORT, () => {
     console.log(`L'application s'exécute sur ${process.env.APP_ADDRESS}`)
