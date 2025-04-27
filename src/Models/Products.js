45b1d5bd44020939e70c9d5948aec6e02b78b09b
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-27 08:32:44 +0300

    définition du modèle de données pour les projets

--- src/Models/Products.js
+++ src/Models/Products.js
@@ -2,6 +2,17 @@ const mongoose = require('mongoose')
 
 const productSchema = mongoose.Schema({
     name: { type: String, required: true },
-    type: { type: String, enum: ["website", "mobile-application", "desktop-application"], required: true }
-    
+    type: { type: String, enum: ["website", "mobile-application", "desktop-application"], required: true },
+    developers: { type: Array, required: true },
+    descriptions: { type: String, required: true},
+    skills: { type: Array, required: true },
+    github_url: { type: String, required: true },
+    movie_file_url: { type: String, required: true },
+    host_url: { type: String, required: true }
 })
+
+const _Products = mongoose.model('Products', productSchema)
+
+module.exports = {
+    Products: _Products
+}
