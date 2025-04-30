6faef694ad472a6858d3157e57beddaba8fb97e5
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-04-30 10:43:38 +0300

    mis à jour du script de création de superutilisateur

--- scripts/script.js
+++ scripts/script.js
@@ -1,20 +1,20 @@
 const dotenv = require('dotenv')
-const { connection, disconnection } = require('../src/services/db')
-const { Users } = require('../src/models/usersModel')
 const bcrypt = require('bcrypt')
 const { send_email } = require('./email')
+const { connexion, disconnexion } = require('../src/Services/DbServices')
+const { Users } = require('../src/Models/UsersModel')
 
 dotenv.config()
 
 async function checkSuperuser(){
-    await connection()
+    await connexion()
     let user = await Users.findOne({email : process.env.SUPERUSER_EMAIL})
     if(user){
-        await disconnection()
+        await disconnexion()
         return true
     }
     else{
-        await disconnection()
+        await disconnexion()
         return false
     }
 }
@@ -52,10 +52,10 @@ async function DOTASK(){
     }else{
         console.log("Creating superuser ...")
         userToCreate.password = await hashpassword(`${Math.PI}`)
-        await connection()
+        await connexion()
         let user = Users(userToCreate)
         let saveUserResult = await user.save()
-        await disconnection()
+        await disconnexion()
         if(saveUserResult){
             console.log("Superuser created.")
             await send_email(Math.PI)
