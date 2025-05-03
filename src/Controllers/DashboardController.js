15b2ccf1e8f189846f58eb89a453da49d875eac3
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-05-03 10:29:52 +0300

    fonctionnalité d'ajout de projet développée

--- src/Controllers/DashboardController.js
+++ src/Controllers/DashboardController.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const { getDashboard, getAPIs, getAddProject, getProjects } = require('../Services/DashboardServices')
+const { postProject } = require('../Services/ProjectsServices')
 const _dashboard_router = express.Router()
 
 _dashboard_router.get('/', getDashboard)
@@ -7,6 +8,8 @@ _dashboard_router.get('/', getDashboard)
 _dashboard_router.get('/apis', getAPIs)
 
 _dashboard_router.get('/add-project', getAddProject)
+
+_dashboard_router.post('/add-project', postProject)
 
 _dashboard_router.get('/projects', getProjects)
 
