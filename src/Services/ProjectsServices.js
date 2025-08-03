78804a1656d9b263bf2c7b23eacaab48c270f36f
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2025-08-03 07:51:02 +0300

    amélioration du service de récupération de liste de projet par les application externes

--- src/Services/ProjectsServices.js
+++ src/Services/ProjectsServices.js
@@ -5,10 +5,7 @@ async function _getAllProjects(request, response) {
     try {
         await connexion()
         let projects = await Projects.find({})
-        response.status(200).json({
-            message: "Liste de tout les projets.",
-            data: projects
-        })
+        response.status(200).json(projects)
     }catch(error){
         console.log({here : error})
     }finally{
