1623ae462930c41829b7bf9dba4cf60e2347d537
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-23 12:27:54 +0300

    redéveloppement du service de connexion à la base de donée

--- src/Services/db.js
+++ src/Services/db.js
@@ -1,40 +1,8 @@
-const mongoose = require('mongoose')
+import chalk from "chalk";
+import { connect } from "mongoose";
 
-async function _connexion() {
-    try {
-        await mongoose.connect(`${process.env.DB_URI}`)
-            .then(()=>{
-                console.log("Database connexion successfully.")
-            })
-            .catch((_error)=>{
-                console.log({
-                    message: "Database connexion failure.",
-                    error: _error 
-                })
-            })
-    }catch(error){
-        console.log(error)
-    }
-}
-
-async function _disconnexion() {
-    try {
-        await mongoose.disconnect()
-            .then(()=>{
-                console.log("Database disconnexion successfully.")
-            })
-            .catch((_error)=>{
-                console.log({
-                    message: "Database disconnexion failure",
-                    error: _error
-                })
-            })
-    }catch(error){
-        console.log(error)
-    }
-}
-
-module.exports = {
-    connexion: _connexion,
-    disconnexion: _disconnexion
+export function DbConnection() {
+    connect(process.env.DB_URI)
+        .then(()=>{ console.log(chalk.bgHex('#098702ff').hex('#fffbfc')('Database connection success.')) })
+        .catch((err)=>{ console.log('Database connection error :' + err) })
 }
