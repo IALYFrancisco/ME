f1219870f8c104c243f3a49081f68559216c7f3d
IALY Francisco Raymond <ialyfrancisco7@gmail.com>
2026-02-23 12:29:52 +0300

    corréction des syntaxes commnon en ES

--- src/Services/db.js
+++ src/Services/db.js
@@ -1,8 +1,12 @@
-import chalk from "chalk";
-import { connect } from "mongoose";
+const chalk = require("chalk")
+const { connect } = require("mongoose");
 
-export function DbConnection() {
+function DbConnection() {
     connect(process.env.DB_URI)
         .then(()=>{ console.log(chalk.bgHex('#098702ff').hex('#fffbfc')('Database connection success.')) })
         .catch((err)=>{ console.log('Database connection error :' + err) })
+}
+
+module.exports = {
+    DbConnection : DbConnection
 }
